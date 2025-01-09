404aab767f0199d171c404c1dffb1c867a550a09
CBSaksager <Christiansaksager@gmail.com>
2025-01-09 12:45:38 +0100

    Input system setup up

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,11 +8,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public InputActionReference movementAction;
 
     public float forwardForce = 250f;
-    public float rotationSpeed = 100f;
-
     public float backwardForce = -250f;
+    public float rotationSpeed = 100f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,30 +23,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector2 input = movementAction.action.ReadValue<Vector2>();
         Vector3 velocity = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (input.y > 0)
         {
             velocity = transform.forward * forwardForce * Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (input.y < 0)
         {
             velocity = transform.forward * backwardForce * Time.deltaTime;
-
         }
 
         rb.linearVelocity = velocity;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (input.x < 0)
         {
             transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (input.x > 0)
         {
             transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
         }
-
     }
 }
