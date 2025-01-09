28bf42e5aaeb3801cce5e70b6048b1d51c51bec2
oscarsvenstrup <129071972+oscarsvenstrup@users.noreply.github.com>
2025-01-09 14:20:16 +0100

    Added bouncing of walls

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -2,11 +2,15 @@ using Unity.VisualScripting;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEditor.Callbacks;
+using System.Runtime.CompilerServices;
 
 public class Bullet : MonoBehaviour
 {
     public float lifeTime = 2;
-    public Rigidbody rigidbody;
+    public Rigidbody rb;
+
+    Vector3 lastVelocity;
 
     private void Update()
     {
@@ -17,10 +21,16 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
 
+        lastVelocity = rb.linearVelocity;
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
+        Debug.Log("hit barrier");
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
 
+        rb.linearVelocity = direction * Mathf.Max(speed, 0f);
     }
 }
