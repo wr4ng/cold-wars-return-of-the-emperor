df380da805af897d6078dc9e991ee7784605e7db
CBSaksager <Christiansaksager@gmail.com>
2025-01-15 11:13:17 +0100

    Added test obj to prefabs

--- Assets/Scripts/Player/PowerUps.cs
+++ Assets/Scripts/Player/PowerUps.cs
@@ -6,14 +6,32 @@ public class PowerUps : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            PickUp();
+            PickUp(other);
         }
     }
 
-    void PickUp()
+    void PickUp(Collider player)
     {
         // Add powerup to player
         Debug.Log("Powerup picked up!");
+
+        // Add powerup to player depending on tag of powerup
+        switch (gameObject.tag)
+        {
+            case "Laser":
+                Debug.Log("Laser powerup picked up!");
+                player.GetComponent<PlayerPowerStats>().hasLaser = true;
+                break;
+
+            case "Mini":
+                Debug.Log("Mini powerup picked up!");
+                player.GetComponent<PlayerPowerStats>().hasMini = true;
+                break;
+
+            default:
+                Debug.Log("Powerup picked up!");
+                break;
+        }
 
         // Remove powerup from scene
         Destroy(gameObject);
