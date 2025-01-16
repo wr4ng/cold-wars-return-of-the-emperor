ccd3fec73d0014eb0b78942fa515f7a19d37da37
oscarsvenstrup <129071972+oscarsvenstrup@users.noreply.github.com>
2025-01-16 12:05:42 +0100

    Implemented laser power up

---
+++ Assets/Scripts/Player/Laser.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    [SerializeField] private LineRenderer lineRenderer;
+    private int numOfReflections = 5;
+    public float range = 50f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lineRenderer.positionCount = numOfReflections + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CastRay(transform.position, transform.forward);
+    }
+
+    private void CastRay(Vector3 laserPos, Vector3 laserDir)
+    {
+        var currentLaserIndex = 0;
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(currentLaserIndex, laserPos);
+
+        for (var i = 0; i < numOfReflections; i++)
+        {
+
+            Ray ray = new Ray(laserPos, laserDir);
+
+            if (Physics.Raycast(ray, out RaycastHit laserHit, range, ~0, QueryTriggerInteraction.Collide))
+            {
+                if (laserHit.collider.CompareTag("Wall"))
+                {
+                    currentLaserIndex++;
+                    lineRenderer.positionCount = currentLaserIndex + 1;
+                    lineRenderer.SetPosition(currentLaserIndex, laserHit.point);
+                    laserDir = Vector3.Reflect(laserDir, laserHit.normal);
+                    laserPos = laserHit.point;
+                }
+                else if (laserHit.collider.CompareTag("Player"))
+                {
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        Destroy(laserHit.collider.gameObject);
+                    }
+                    currentLaserIndex++;
+                    lineRenderer.positionCount = currentLaserIndex + 1;
+                    lineRenderer.SetPosition(currentLaserIndex, laserPos + laserDir * range);
+                    break;
+                }
+            }
+            else
+            {
+                currentLaserIndex++;
+                lineRenderer.positionCount = currentLaserIndex + 1;
+                lineRenderer.SetPosition(currentLaserIndex, laserPos + laserDir * range);
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            CastRay(transform.position, transform.forward);
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.Rendering;
 
 public class PlayerShooting : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class PlayerShooting : MonoBehaviour
     public float bulletSpeed = 10f;
     public float fireRate = 0.5f;
     private float timer;
+
 
     [SerializeField]
     private InputActionReference shootAction;
@@ -29,31 +31,25 @@ public class PlayerShooting : MonoBehaviour
 
         if (shootAction.action.triggered && timer <= 0)
         {
-            switch (true)
-            {
-                case bool _ when powerUp.hasLaser:
-                    Laser();
-                    break;
 
-                case bool _ when powerUp.hasMini:
-                    Mini();
-                    break;
+            Laser();
+            // switch (true)
+            // {
+            //     case bool _ when powerUp.hasLaser:
+            //         Laser();
+            //         break;
 
-                default:
-                    Shoot();
-                    break;
-            }
+            //     case bool _ when powerUp.hasMini:
+            //         Mini();
+            //         break;
+
+            //     default:
+            //         Shoot();
+            //         break;
+            // }
         }
 
-        // if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 20f))
-        // {
-        //     Debug.Log("Blaster hit!");
-        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfo.distance, Color.green);
-        // }
-        // else
-        // {
-        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f, Color.red);
-        // }
+
     }
 
     void Shoot()
@@ -66,8 +62,7 @@ public class PlayerShooting : MonoBehaviour
 
     void Laser()
     {
-        Debug.Log("Laser");
-        timer = 1;
+
     }
 
     void Mini()
